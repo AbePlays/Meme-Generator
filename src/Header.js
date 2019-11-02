ac4f3a5d612959b8548b53da166b348bcf7d2cce
Abe <abhi.rawat456@gmail.com>
2019-11-02 22:07:10 +0530

    Sorting the header css

--- src/Header.js
+++ src/Header.js
@@ -8,6 +8,10 @@ function Header() {
                 alt="Problem?"
             />
             <p>Meme Generator</p>
+            <img 
+                src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png" 
+                alt="Problem?"
+            />
         </header>
     )
 }
